9b2e4bf6175146c3e38a61bc94730282226c7d46
Denis <denis.zol@code-care.pro@gmail.com>
2022-08-30 02:05:10 +0300

    add filter, improve fonts

--- client/src/Components/Room/RoomList/RoomList.tsx
+++ client/src/Components/Room/RoomList/RoomList.tsx
@@ -1,24 +1,32 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useState} from 'react';
 import {RoomModel} from "../../../Models/Room.model";
 
 import RoomItem from "../RoomItem/RoomItem";
 import Button from "../../Button/Button";
 import styles from "./RoomList.module.scss"
 import ScrollToBottom from "react-scroll-to-bottom";
+import search from "../../../Styles/Images/search.svg"
+
 
 const RoomList = ({
                       rooms,
                       setSelectRoom,
                       callback,
                       callbackDelete
-                  }: { rooms: RoomModel[] | undefined, setSelectRoom: any, callback: any, callbackDelete: any }) => {
+                  }: { rooms: RoomModel[], setSelectRoom: any, callback: any, callbackDelete: any }) => {
+
+    const [filteredString, setFilteredString] = useState<string>("");
 
     return (
         <>
+            <div className={styles.searchWrapper}>
+                <label className={styles.searchLabel}>Search:</label>
+                <input className={styles.search} type="text" onChange={(event) => setFilteredString(event.target.value)}/>
+            </div>
             {rooms &&
                 <ScrollToBottom className={styles.wrapper} followButtonClassName={styles.followButtonClassName}>
                     <div className={styles.rooms}>
-                        {rooms.map(room =>
+                        {rooms.filter(room => room.name.includes(filteredString)).map(room =>
                             <RoomItem callback={setSelectRoom} callbackDelete={callbackDelete} room={room}
                                       key={room.uuid}/>
                         )}
